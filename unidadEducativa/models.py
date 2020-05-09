7ba1fa5d83808a8fda5d6409ae0a163e48d3658f
Julio Torres <juliobtorresv@gmail.com>
2020-05-09 00:23:36 -0500

    Clase UEs

--- unidadEducativa/models.py
+++ unidadEducativa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class UnidadEducativa(models.Model):
+    ruc=models.CharField(max_length=15)
+    nombre=models.CharField(max_length=40)
+    direccion=models.CharField(max_length=15)
+    email=models.EmailField(blank=True,null=True)
+    telefono=models.CharField(max_length=15)
+    
